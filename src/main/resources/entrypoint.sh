90879ccf8caa8672dd4e9e6fca9eb57ad733c532
Dwight Frye <dwight@openweave.org>
2021-02-08 17:53:56 -0500

    Get docker container building correctly

--- src/main/resources/entrypoint.sh
+++ src/main/resources/entrypoint.sh
@@ -39,5 +39,5 @@ fi
 if [ "$1" == "/bin/bash" ]; then
   exec /bin/sh
 else
-  exec java ${DEBUG_ARGS} -jar planez-server.jar server configuration.ftl
+  exec java ${DEBUG_ARGS} -jar groundschool.jar server configuration.ftl
 fi
